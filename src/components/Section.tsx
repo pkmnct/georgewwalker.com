906724826e8519b665a08c1c49dd7bdd37572eb2
George W. Walker <george@georgewwalker.com>
2019-09-20 21:23:16 -0500

    Add styles to section

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
 const Section = styled.section`
-  display: flex;
+  display: block;
   flex-grow: 1;
+  width: calc(100% - 4em);
+  margin: 2em;
+  margin-bottom: 0;
+  transform: translateY(100%);
+  transition: transform ease-in-out 0.5s;
+  background: rgba(255, 255, 255, 0.8);
+  color: #000;
+  padding: 1em;
+  &.in {
+    transform: none;
+  }
 `;
 
 export default Section;
